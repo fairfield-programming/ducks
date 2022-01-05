051d8a51d3ed5a0143d6e1daf8df3f007deba6fa
William McGonagle <wpmcgonagle@gmail.com>
2022-01-05 13:20:19 -0500

    📝 Added Code

---
+++ src/SVGUtilities.js
@@ -0,0 +1,13 @@
+function stripParentTags(input) {
+
+    // Set Output to Input
+    let output = input; 
+
+    // Cut Out Unnecessary Stuff
+    output = output.replace(/<svg[\sa-zA-Z0-9\=\"\:\/\.]*>/g, "");
+    output = output.replace(/<\/svg>/g, "");
+    
+    // Return the Trimmed Output
+    return output.trim();
+
+}
